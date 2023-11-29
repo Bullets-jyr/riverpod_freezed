9e52f31e9e382390991dbddf1261dbb2a51db0c0
Bullets-jyr <jyr@bullets.co.kr>
2023-11-29 19:53:29 +0900

    10. Freezed - Basics

---
+++ lib/models/person.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+// dart run build_runner watch -d
+part 'person.freezed.dart';
+
+@freezed
+class Person with _$Person {
+  const factory Person({
+    required int id,
+    required String name,
+    required String email,
+  }) = _Person;
+}
